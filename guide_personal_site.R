13864c8e09cb5f3205157333968fd9078f405033
Miguel A. Sorrel <miguel.sorrel@uam.es>
2020-07-01 11:56:25 +0200

    Third commit

---
+++ guide_personal_site.R
@@ -0,0 +1,24 @@
+# info from: https://livefreeordichotomize.com/2017/08/08/how-to-make-an-rmarkdown-website/
+
+# update rmarkdown
+install.packages("rmarkdown", type = "source")
+
+# Step 1: Git on with it
+# create new repository
+
+# Step 2: 
+# Open RStudio and select “New Project” under the “File” menu item. If you are having some trouble here, check out Jenny’s delightful Happy git with R tutorial.A New Project box should pop up - click “Version Control”.
+
+# Step 3: Start your Markdowns!  
+# Under “File” select “New File” then “R Markdown” - save this file as “index.Rmd”.
+# Under “File” select “New File” then “R Markdown” - save this file as “about.Rmd”.
+# Under “File” select “New File” then “Text File” - save this file as “_site.yml“.
+
+# Step 4: 
+rmarkdown::render_site()
+
+# Step 5: Git it hosted
+# at Github.com settings source < Master bracnh
+
+https://miguel-sorrel.github.io/personal_site/
+
